8732ee855ab3f02bc9923ef51ce54df3fbbcd190
Ahmed Jellouli <ahmed.jellouli@epfl.ch>
2022-03-04 18:45:23 +0100

    Adapt swissparlpy/__init__.py to include the function get_data_batched

--- swissparlpy/__init__.py
+++ swissparlpy/__init__.py
@@ -1,11 +1,11 @@
 """Client for Swiss parliament API"""
 
-__version__ = '0.2.1'
-__all__ = ['client', 'errors']
+__version__ = "0.2.1"
+__all__ = ["client", "errors"]
 
 from .errors import SwissParlError  # noqa
 from .client import SwissParlClient
-from pyodata.v2.service import GetEntitySetFilter as filter # noqa
+from pyodata.v2.service import GetEntitySetFilter as filter  # noqa
 
 
 def get_tables():
@@ -31,3 +31,8 @@ def get_glimpse(table, rows=5):
 def get_data(table, filter=None, **kwargs):  # noqa
     client = SwissParlClient()
     return client.get_data(table, filter, **kwargs)
+
+
+def get_data_batched(table, filter=None, batch_size=500000, **kwargs):  # noqa
+    client = SwissParlClient()
+    return client.get_data_batched(table, filter, batch_size, **kwargs)
