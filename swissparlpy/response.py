b61a5af338bcae312bda43d5ef0fb609e5fa782e
Ahmedjjj <ahmed.jellouli@epfl.ch>
2022-06-14 14:54:47 +0200

    Delete unused abstractions, as request execution is moved into the client

--- swissparlpy/response.py
+++ swissparlpy/response.py
@@ -24,52 +24,6 @@ class SwissParlResponse:
         return {k: v() for k, v in items.items()}
 
 
-class SwissParlRequestResponse(SwissParlResponse):
-    def __init__(self, entity_request, variables):
-        entities = entity_request.execute()
-        super().__init__(entities, variables)
-
-
-class SwissParlBatchedResponse(SwissParlResponse):
-    def __init__(
-        self, entity_requests, variables, retries=10, use_disk=False, save_loc="."
-    ) -> None:
-        entities = []
-        self.savefiles = []
-        for i, entity_request in tqdm.tqdm(enumerate(entity_requests)):
-            batch_entities = self._execute_and_retry(entity_request, retries)
-            logger.debug("Batch %i successful", i)
-            if use_disk:
-                file_path = os.path.join(save_loc, f"batch{i}")
-                with open(file_path, "w") as file:
-                    json.dump(
-                        [
-                            {k: str(getattr(entity, k)) for k in variables}
-                            for entity in batch_entities
-                        ],
-                        file,
-                    )
-                self.savefiles.append(file_path)
-            else:
-                entities.extend(batch_entities)
-
-        super().__init__(entities, variables)
-
-    def _execute_and_retry(self, request, retries):
-        trials = 0
-        while trials < retries:
-            try:
-                return request.execute()
-            except ConnectionError:
-                logger.debug("Retrying request... num retries: %i", trials)
-                trials += 1
-            except pyodata.exceptions.HttpError:
-                logger.info("HTTP error, retrying...num retries: %i", trials)
-                trials += 1
-
-        raise SwissParlError("Could not execute request after %i retries", retries)
-
-
 class SwissParlDataProxy(object):
     def __init__(self, proxy, attribute):
         self.proxy = proxy
