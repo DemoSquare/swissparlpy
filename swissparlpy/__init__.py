8c217bc691f3d677ce3f9541605fc8678acd5853
Ahmedjjj <ahmed.jellouli@epfl.ch>
2022-06-14 15:08:29 +0200

    Modify __init__.py to use default batching of 1000

--- swissparlpy/__init__.py
+++ swissparlpy/__init__.py
@@ -4,40 +4,41 @@ __version__ = "0.2.1"
 __all__ = ["client", "errors"]
 
 from .errors import SwissParlError  # noqa
-from .client import SwissParlClient
+from .client import SwissParlClient, BatchSwissParlClient
 from pyodata.v2.service import GetEntitySetFilter as filter  # noqa
+
+SERVICE_URL = "https://ws.parlament.ch/odata.svc/"
+
+client = BatchSwissParlClient(url=SERVICE_URL, batch_size=1000, retries=10)
+
+
+def set_batch_size(size):
+    client.batch_size = size
+
+
+def set_retries(retries):
+    client.retries = retries
 
 
 def get_tables():
-    client = SwissParlClient()
     return client.get_tables()
 
 
 def get_variables(table):
-    client = SwissParlClient()
     return client.get_variables(table)
 
 
 def get_overview():
-    client = SwissParlClient()
     return client.get_overview()
 
 
 def get_glimpse(table, rows=5):
-    client = SwissParlClient()
     return client.get_glimpse(table, rows)
 
 
 def get_data(table, filter=None, **kwargs):  # noqa
-    client = SwissParlClient()
     return client.get_data(table, filter, **kwargs)
 
 
-def get_data_batched(table, filter=None, batch_size=50000, **kwargs):  # noqa
-    client = SwissParlClient()
-    return client.get_data_batched(table, filter, batch_size, **kwargs)
-
-
 def get_count(table, filter=None, **kwargs):  # noqa
-    client = SwissParlClient()
     return client.get_count(table, filter, **kwargs)
